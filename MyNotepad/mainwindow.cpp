6330aedc1007675422e293de98a6fa24a7b52d31
Mathieu Delehaye <mathieu.delehaye@gmail.com>
2021-01-01 11:12:42 +0000

    First commit

---
+++ MyNotepad/mainwindow.cpp
@@ -0,0 +1,16 @@
+#include "mainwindow.h"
+#include "ui_mainwindow.h"
+
+MainWindow::MainWindow(QWidget *parent)
+    : QMainWindow(parent)
+    , ui(new Ui::MainWindow)
+{
+    ui->setupUi(this);
+    ui->menubar->setNativeMenuBar(false);
+}
+
+MainWindow::~MainWindow()
+{
+    delete ui;
+}
+
